dc33405b849164b4d00fe6c219532b44d37460e1
Zaksley <enzo8550@gmail.com>
2022-02-18 22:45:05 +0100

    Add player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 4.5f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            // Movement Input 
+        Vector2 moveDirection = Vector2.zero; 
+        var vertical = Input.GetAxisRaw("Vertical"); 
+        var horizontal = Input.GetAxisRaw("Horizontal"); 
+
+        if (horizontal > 0) moveDirection.x += 1; 
+        if (horizontal < 0) moveDirection.x += -1; 
+
+        if (vertical > 0) moveDirection.y += 1; 
+        if (vertical < 0) moveDirection.y += -1; 
+
+        moveDirection = moveDirection.normalized; 
+        transform.Translate(moveDirection * speed * Time.deltaTime);
+    }
+}
